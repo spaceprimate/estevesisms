b3144ce57f05eaeed8eb57c165f9b3fee0a0c46f
dm <d4nielmurphy@gmail.com>
2016-12-13 21:42:29 -0600

    site is ready

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.controller('estevezController', ['$http', function($http){
 
 	this.showPunchline = false;
 
+	this.bgClass = "tile0";
+
+
+	this.updateBg = function (){
+		$('body').removeClass(this.bgClass);
+		this.bgClass = "tile" + getRandomInt(0, 6);
+		$('body').addClass(this.bgClass);
+	};
+
+
+
 	//store.products = [];
 	$http.get('jokes.json').success(function(data){
 		jokes.jokes = data;
@@ -24,14 +35,26 @@ app.controller('estevezController', ['$http', function($http){
 		var numJokes = jokes.jokes.length;
 		var randJoke = getRandomInt(0, numJokes);
 		this.currentJoke = jokes.jokes[randJoke];
+		this.updateBg();
 	};
 
 	//init
 	this.getJoke();
 } ]);
 
+	/* this is getting excessive */
+	/*
+	document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var dot, eventDoc, doc, body, pageX, pageY;
+        $('body').css("background-position", (event.screenX / 10) + "px " + (event.screenY / 10 ) + "px"  );
+    }
+    */
+
 
 })();
+
+
 
 // Returns a random integer between min (included) and max (excluded)
 // Using Math.round() will give you a non-uniform distribution!
